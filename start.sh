6da0148ac95106a80b78178ae58826274c8982e2
boris <boris@brzr.fr>
2014-09-18 08:02:41 +0200

    Verbose name of image.

--- start.sh
+++ start.sh
@@ -23,7 +23,7 @@ else
 fi
 
 # 1. Create image with roundcube
-$DOCKER build -t roundcube ./roundcube
+$DOCKER build -t thinred/roundcube ./roundcube
 
 # 2. Start it and attach rc-data volumes
-$DOCKER run -p 127.0.0.1:$PORT:80 --volumes-from roundcube-data -d --name roundcube roundcube
+$DOCKER run -p 127.0.0.1:$PORT:80 --volumes-from roundcube-data -d --name roundcube thinred/roundcube
