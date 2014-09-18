a97a03d9d478c47cb7b178f5137c8fb25c60aaae
boris <boris@brzr.fr>
2014-09-18 08:01:29 +0200

    Explicit python version (Arch Linux defaults to Python3)

--- gen-key.sh
+++ gen-key.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-HASH=$(python -c 'print open("/dev/urandom").read(24).encode("hex")')
+HASH=$(python2 -c 'print open("/dev/urandom").read(24).encode("hex")')
 sed -i 's/RANDOM_KEY/'$HASH'/g' ./roundcube/config.inc.php
