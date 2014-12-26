b3a50cba0db385c1c8ae2426668e05c99c741a4b
uhumph <uhumph@slo-computer.com>
2014-12-26 14:02:34 -0800

    Update start.sh
    Small refactor, handle errors, etc...

--- start.sh
+++ start.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 
-set -eux
-
-DOCKER=docker
 PORT=${PORT:-8081}
 
-# hack for debian
-if which docker.io; then
-    DOCKER=docker.io
-fi
+# determine the docker executable name
+if which docker; then DOCKER='docker'
+else DOCKER='docker.io' ; fi # debian default
+
+# make output verbose
+set -o xtrace -o nounset 
 
 # 0. Create roundcube-data container to host only data
-set +e
-$DOCKER ps -a | grep roundcube-data
-RESULT=$?
-set -e
-
-if [ $RESULT = 0 ]; then
-    true
-else
-    $DOCKER run -v /rc --name roundcube-data busybox mkdir -p /rc
+sudo $DOCKER ps -a | grep roundcube-data
+if [ "$?" -ne 0 ] ; then 
+    sudo $DOCKER run -v /rc --name roundcube-data busybox mkdir -p /rc
 fi
 
 # 1. Create image with roundcube
-$DOCKER build -t thinred/roundcube ./roundcube
+sudo $DOCKER build -t thinred/roundcube ./roundcube
 
 # 2. Start it and attach rc-data volumes
-$DOCKER run -p 127.0.0.1:$PORT:80 --volumes-from roundcube-data -d --name roundcube thinred/roundcube
+sudo $DOCKER run -p 127.0.0.1:$PORT:80 --volumes-from roundcube-data -d --name roundcube thinred/roundcube
