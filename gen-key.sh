af8a22755e1a7df15f84af079baddc13a3259226
uhumph <uhumph@slo-computer.com>
2014-12-26 12:03:36 -0800

    Update gen-key.sh
    Removed python2 requirement from gen-key.sh.

--- gen-key.sh
+++ gen-key.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-HASH=$(python2 -c 'print open("/dev/urandom").read(24).encode("hex")')
+HASH=$(cat /dev/urandom | tr -dc 'a-f0-9' | fold -w 48 | head -n 1)
 sed -i 's/RANDOM_KEY/'$HASH'/g' ./roundcube/config.inc.php
