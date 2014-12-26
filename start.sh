3a546bf46bcb3c07d34e029f5a8b761bdf5fe252
uhumph <uhumph@slo-computer.com>
2014-12-26 14:56:37 -0800

    Update start.sh
    Added more factoring and more errors.

--- start.sh
+++ start.sh
@@ -6,12 +6,18 @@ PORT=${PORT:-8081}
 if which docker; then DOCKER='docker'
 else DOCKER='docker.io' ; fi # debian default
 
+# check that docker is installed
+if sudo $DOCKER --version ; then
+    echo 'install docker first...' ; exit 1
+fi
+
 # make output verbose
 set -o xtrace -o nounset 
 
 # 0. Create roundcube-data container to host only data
-sudo $DOCKER ps -a | grep roundcube-data
-if [ "$?" -ne 0 ] ; then 
+sudo $DOCKER ps -a | grep 'roundcube-data'
+if [ "$?" -ne 0 ] ; then
+    echo 'launching instance of roundcude-data'
     sudo $DOCKER run -v /rc --name roundcube-data busybox mkdir -p /rc
 fi
 
